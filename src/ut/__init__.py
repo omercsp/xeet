3b13993cbdb0c39057b908548209f2cfdc62dbc1
Omer Caspi <omer@viridian.earth>
2024-09-19 16:23:14 +0300

    xeet: rehaul
    Signed-off-by: Omer Caspi <omer@viridian.earth>

---
+++ src/ut/__init__.py
@@ -0,0 +1,218 @@
+from xeet.log import init_logging, log_info
+from xeet.pr import mute_prints, pr_obj, DictPrintType
+from xeet.common import XeetVars
+from xeet.core import run_tests
+from xeet.driver import Driver, TestCriteria, xeet_init
+from xeet.xtest import TestResult, Xtest, XStepListResult
+from xeet.xstep import XStepResult
+from xeet import RunReporter
+from tempfile import gettempdir
+from dataclasses import dataclass, field
+from typing import ClassVar, Any, Iterable
+import yaml
+import json
+import os
+import unittest
+import tempfile
+
+
+_log_file = os.path.join(gettempdir(), "xeet_ut.log")
+init_logging("xeet", _log_file, False)
+mute_prints()
+
+
+@dataclass
+class ConfigTestWrapper:
+    name: str
+    includes: list[str] = field(default_factory=list)
+    tests: list[dict] = field(default_factory=list)
+    variables: dict[str, Any] = field(default_factory=dict)
+    settings: dict[str, Any] = field(default_factory=dict)
+
+    file_path: str = ""
+    _xeet_dir: ClassVar[tempfile.TemporaryDirectory] = None  # type: ignore
+
+    def __post_init__(self):
+        if not ConfigTestWrapper._xeet_dir:
+            raise RuntimeError("xeet dir not initialized. "
+                               "Did you forget to call ConfigTestWrapper.init_xeet_dir?")
+
+        if os.path.isabs(self.name):
+            self.file_path = self.name
+        else:
+            self.file_path = os.path.join(ConfigTestWrapper._xeet_dir.name, self.name)
+        log_info(self.file_path)
+
+    @property
+    def desc(self) -> dict:
+        return {
+            "include": self.includes,
+            "tests": self.tests,
+            "variables": self.variables,
+            "settings": self.settings,
+        }
+
+    def save(self, show: bool = False) -> None:
+        file_suffix = os.path.splitext(self.file_path)[1]
+        log_info(f"Saving config to '{self.file_path}'")
+        with open(self.file_path, 'w') as f:
+            if file_suffix == ".yaml" or file_suffix == ".yml":
+                f.write(yaml.dump(self.desc))
+                if show:
+                    pr_obj(self.desc, pr_func=print, print_type=DictPrintType.YAML)
+            elif file_suffix == ".json":
+                f.write(json.dumps(self.desc))
+                if show:
+                    pr_obj(self.desc, pr_func=print)
+            else:
+                raise ValueError(f"Unsupported file format '{file_suffix}'")
+
+    @staticmethod
+    def config_set(func):
+        def _inner(self, *args, **kwargs):
+            if kwargs.pop("reset", False):
+                self._reset()
+            save = kwargs.pop("save", False)
+            show = kwargs.pop("show", False)
+            ret = func(self, *args, **kwargs)
+            if save:
+                self.save()
+            if show:
+                pr_obj(self.desc, pr_func=print, print_type=DictPrintType.YAML)
+            return ret
+        return _inner
+
+    @config_set
+    def add_test(self, name, **kwargs) -> dict:
+        desc = {"name": name, **kwargs}
+        self.tests.append(desc)
+        return desc
+
+    @config_set
+    def add_var(self, name: str, value: Any, **_) -> None:
+        self.variables[name] = value
+
+    @config_set
+    def add_include(self, name: str) -> None:
+        self.includes.append(name)
+
+    @config_set
+    def add_settings(self, name: str, value: Any, **_) -> None:
+        self.settings[name] = value
+
+    def _reset(self):
+        self.tests.clear()
+        self.variables.clear()
+        self.includes.clear()
+        self.settings.clear()
+
+    @staticmethod
+    def init_xeet_dir():
+        ConfigTestWrapper._xeet_dir = tempfile.TemporaryDirectory()
+
+    @staticmethod
+    def fini_xeet_dir():
+        if ConfigTestWrapper._xeet_dir:
+            ConfigTestWrapper._xeet_dir.cleanup()
+
+
+def test_output_file(self, name: str) -> str:
+    return os.path.join(self.path, name)
+
+
+def project_root() -> str:
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+
+def tests_utils_command(name: str, *args) -> str:
+    path = os.path.join(project_root(), "scripts", "testing", name)
+    ret = f"python3 {path}"
+    if not args:
+        return ret
+    args = " ".join(args)
+    return f"{ret} {args}"
+
+
+def ref_str(var_name: str) -> str:
+    return f"{XeetVars._REF_PREFIX}{var_name}"
+
+
+class XeetUnittest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        ConfigTestWrapper.init_xeet_dir()
+        cls.main_config_wrapper = ConfigTestWrapper("main.json")
+        cls.reporter = RunReporter(1)
+        cls.criteria = TestCriteria([], [], [], [], False)
+
+    @classmethod
+    def tearDownClass(cls):
+        ConfigTestWrapper.fini_xeet_dir()
+
+    @classmethod
+    def add_var(cls, name: str, value: Any, **kwargs) -> None:
+        cls.main_config_wrapper.add_var(name, value, **kwargs)
+
+    def gen_xvars(self) -> XeetVars:
+        return XeetVars(self.main_config_wrapper.variables)
+
+    @classmethod
+    def add_test(cls, name: str, **kwargs) -> None:
+        cls.main_config_wrapper.add_test(name, **kwargs)
+
+    @classmethod
+    def add_include(cls, name: str, **kwargs) -> None:
+        cls.main_config_wrapper.add_include(name, **kwargs)
+
+    @classmethod
+    def add_setting(cls, name: str, value: Any, **kwargs) -> None:
+        cls.main_config_wrapper.add_settings(name, value, **kwargs)
+
+    @classmethod
+    def run_test(cls, name: str) -> TestResult:
+        cls.criteria.names = {name}
+        run_info = run_tests(cls.main_config_wrapper.file_path, cls.criteria, cls.reporter)
+        res = run_info.test_result(name, 0)
+        return res
+
+    @classmethod
+    def run_tests_list(cls, names: Iterable[str]) -> Iterable[TestResult]:
+        cls.criteria.names = set(names)
+        run_info = run_tests(cls.main_config_wrapper.file_path, cls.criteria, cls.reporter)
+        for name in names:
+            yield run_info.test_result(name, 0)
+
+    @classmethod
+    def get_test(cls, name) -> Xtest:
+        return cls.driver().xtest(name)  # type: ignore
+
+    @classmethod
+    def driver(cls) -> Driver:
+        return xeet_init(cls.main_config_wrapper.file_path)
+
+    def assertStepResultEqual(self, res: XStepResult, expected: XStepResult) -> None:
+        self.assertEqual(res.failed, expected.failed)
+        self.assertEqual(res.completed, expected.completed)
+
+    def assertStepResultListEqual(self, res: XStepListResult | None,
+                                  expected: XStepListResult | None) -> None:
+        if expected is None:
+            self.assertIsNone(res)
+            return
+
+        self.assertIsNotNone(res)
+        assert res is not None  # For the type checker
+        self.assertEqual(res.completed, expected.completed)
+        self.assertEqual(res.failed, expected.failed)
+        self.assertEqual(len(res.results), len(expected.results))
+        for r0, r1 in zip(res.results, expected.results):
+            self.assertStepResultEqual(r0, r1)  # type: ignore
+
+    def assertTestResultEqual(self, res: TestResult, expected: TestResult) -> None:
+        self.assertEqual(res.status, expected.status)
+        self.assertStepResultListEqual(res.pre_run_res, expected.pre_run_res)
+        self.assertStepResultListEqual(res.run_res, expected.run_res)
+        self.assertStepResultListEqual(res.post_run_res, expected.post_run_res)
+
+
+__all__ = ["unittest"]
--- src/xeet/__main__.py
+++ src/xeet/__main__.py
@@ -1,25 +1,29 @@
 from xeet import xeet_version
-from xeet.config import Config
-from xeet.common import XeetException, XEET_NO_TOKEN, XEET_YES_TOKEN
-from xeet.log import init_logging, log_error
-from xeet.pr import pr_bright, set_no_color_print
-from xeet.actions import *
-import sys
+from xeet.common import XeetException
+from xeet.log import init_logging, log_error, log_info
+from xeet.pr import pr_header, disable_colors
+from xeet.core import SchemaType
+import xeet.cli as actions
+
+import os
 import argparse
 import argcomplete
 
 
-RUN_CMD = "run"
-LIST_CMD = "list"
-GROUPS_CMD = "groups"
-INFO_CMD = "info"
-DUMP_XTEST_CMD = "dump"
-DUMP_CONFIG_CMD = "dump_config"
-DUMP_SCHEMA_CMD = "dump_schema"
+_RUN_CMD = "run"
+_LIST_CMD = "list"
+_GROUPS_CMD = "groups"
+_INFO_CMD = "info"
+_DUMP_CMD = "dump"
+_DUMP_TEST_CMD = "test"
+_DUMP_SCHEMA_CMD = "schema"
+_DUMP_CONFIG_CMD = "config"
+_YES_TOKEN = 'yes'
+_NO_TOKEN = 'no'
 
 
 def parse_arguments() -> argparse.Namespace:
-    yes_no: list[str] = [XEET_NO_TOKEN, XEET_YES_TOKEN]
+    yes_no: list[str] = [_NO_TOKEN, _YES_TOKEN]
 
     parser = argparse.ArgumentParser(prog='xeet')
     parser.add_argument('--version', action='version', version=f'v{xeet_version}')
@@ -27,11 +31,12 @@ def parse_arguments() -> argparse.Namespace:
     parser.add_argument('--no-splash', action='store_true',
                         default=False, help='don\'t show splash')
 
-    common_parser = argparse.ArgumentParser(add_help=False)
-    common_parser.add_argument('-v', '--verbose', action='count',
-                               help='log file verbosity', default=0)
-    common_parser.add_argument('-c', '--conf', metavar='CONF', help='configuration file to use',
-                               default="xeet.json")
+    conf_file_parser = argparse.ArgumentParser(add_help=False)
+    conf_file_parser.add_argument('-c', '--conf', metavar='CONF', help='configuration file to use')
+
+    common_parser = argparse.ArgumentParser(add_help=False, parents=[conf_file_parser])
+    common_parser.add_argument('-v', '--verbose', action='count', help='log file verbosity',
+                               default=0)
     common_parser.add_argument('--log-file', metavar='FILE', help='set log file', default=None)
 
     test_groups_parser = argparse.ArgumentParser(add_help=False)
@@ -45,10 +50,10 @@ def parse_arguments() -> argparse.Namespace:
     subparsers = parser.add_subparsers(help='commands', dest='subparsers_name')
     subparsers.required = True
 
-    run_parser = subparsers.add_parser(RUN_CMD, help='run a test',
+    run_parser = subparsers.add_parser(_RUN_CMD, help='run a test',
                                        parents=[common_parser, test_groups_parser])
-    run_parser.add_argument('-t', '--test-name', metavar='TESTS', default=[],
-                            help='test names', action='append')
+    run_parser.add_argument('-t', '--test', metavar='TEST', default=[], help='test name',
+                            action='append')
     run_parser.add_argument('--debug', action='store_true', default=False,
                             help='run tests in debug mode')
     run_parser.add_argument('-r', '--repeat', metavar='COUNT', default=1, type=int,
@@ -65,81 +70,93 @@ def parse_arguments() -> argparse.Namespace:
     run_parser.add_argument('-V', '--variable', metavar='VAR', default=[], action='append',
                             help='set a variable')
 
-    info_parser = subparsers.add_parser(INFO_CMD, help='show test info', parents=[common_parser])
+    info_parser = subparsers.add_parser(_INFO_CMD, help='show test info', parents=[common_parser])
     info_parser.add_argument('-t', '--test-name', metavar='TEST', default=None,
                              help='set test name', required=True)
     info_parser.add_argument('-x', '--expand', help='expand values', action='store_true',
                              default=False)
 
-    dump_parser = subparsers.add_parser(DUMP_XTEST_CMD, help='dump a test',
-                                        parents=[common_parser])
-    dump_parser.add_argument('-t', '--test-name', metavar='TEST', default=None,
-                             help='set test name', required=True)
-
-    dump_parser.add_argument('-i', '--includes', help='with inclusions',
-                             action='store_true', default=False)
-
-    list_parser = subparsers.add_parser(LIST_CMD, help='list tests',
+    list_parser = subparsers.add_parser(_LIST_CMD, help='list tests',
                                         parents=[common_parser, test_groups_parser])
     list_parser.add_argument('-a', '--all', action='store_true', default=False,
-                             help='show hidden and shadowed tests')
+                             help='show hidden tests')
     list_parser.add_argument('--names-only', action='store_true', default=False,
                              help=argparse.SUPPRESS)
 
-    subparsers.add_parser(GROUPS_CMD, help='list groups',
+    subparsers.add_parser(_GROUPS_CMD, help='list groups',
                           parents=[common_parser, test_groups_parser])
-    dump_parser = subparsers.add_parser(DUMP_SCHEMA_CMD, help='dump configuration file schema',
-                                        parents=[common_parser])
-    dump_parser.add_argument('-s', '--schema', choices=DUMP_TYPES, default=DUMP_TYPES[0])
 
-    subparsers.add_parser(DUMP_CONFIG_CMD, help='dump configuration')
+    dump_parser = subparsers.add_parser(_DUMP_CMD, help='dump a test descriptor')
+    dump_subparsers = dump_parser.add_subparsers(help='dump commands', dest='dump_type')
+    dump_subparsers.required = True
+    dump_test_parser = dump_subparsers.add_parser(_DUMP_TEST_CMD, help='dump test descriptor',
+                                                  parents=[conf_file_parser])
+    dump_test_parser.add_argument("-t", "--test-name", help="test name", required=True)
+
+    dump_schema_parser = dump_subparsers.add_parser(_DUMP_SCHEMA_CMD, help='dump schema')
+    dump_schema_parser.add_argument('-t', '--type', choices=[s.value for s in SchemaType],
+                                    default=SchemaType.CONFIG.value, help='schema type')
+
+    dump_config_parser = dump_subparsers.add_parser(_DUMP_CONFIG_CMD, help='dump config',
+                                                    parents=[conf_file_parser])
+    dump_config_parser.add_argument('-p', '--path', help='dump path', default=None)
 
     argcomplete.autocomplete(parser, always_complete_options=False)
     args = parser.parse_args()
-    if args.subparsers_name == RUN_CMD and \
-            args.test_name and \
-            (args.group or args.require_group or args.exclude_group):
-        parser.error("test name and groups are mutually exclusive")
+    if args.subparsers_name == _RUN_CMD:
+        if args.test and (args.group or args.require_group or args.exclude_group):
+            parser.error("test name and groups are mutually exclusive")
+        if args.repeat < 1:
+            parser.error("repeat count must be a psitive integer")
     return args
 
 
 def xrun() -> int:
     args = parse_arguments()
     if args.no_colors:
-        set_no_color_print()
-
-    if not args.no_splash:
-        title = f"Xeet, v{xeet_version}"
-        pr_bright(f"{title}\n{'=' * len(title)}\n")
+        disable_colors()
 
     try:
-        log_ok, err_msg = init_logging(args.log_file, args.verbose)
-        if not log_ok:
-            raise XeetException(err_msg)
         cmd_name = args.subparsers_name
-        if cmd_name == DUMP_SCHEMA_CMD:
-            dump_schema(args.schema)
+        if cmd_name == _DUMP_CMD:
+            if args.dump_type == _DUMP_SCHEMA_CMD:
+                actions.dump_schema(args.type)
+            elif args.dump_type == _DUMP_TEST_CMD:
+                actions.dump_test(args.conf, args.test_name)
+            elif args.dump_type == _DUMP_CONFIG_CMD:
+                actions.dump_config(args.conf, args.path)
             return 0
-
-        expand = cmd_name == RUN_CMD or (cmd_name == INFO_CMD and args.expand)
-        config = Config(args, expand)
-        if config.main_cmd == RUN_CMD:
-            return run_test_list(config)
-        elif config.main_cmd == LIST_CMD:
-            list_tests(config)
-        elif config.main_cmd == GROUPS_CMD:
-            list_groups(config)
-        elif config.main_cmd == INFO_CMD:
-            show_test_info(config)
-        elif config.main_cmd == DUMP_CONFIG_CMD:
-            dump_config(config)
-        elif config.main_cmd == DUMP_XTEST_CMD:
-            dump_test(args.test_name, config)
+        title = f"xeet, v{xeet_version}"
+        if not args.no_splash:
+            pr_header(f"{title}\n{'=' * len(title)}\n")
+        if args.log_file:
+            init_logging(title, args.log_file, args.verbose)
+        log_info(f"Running command '{args.subparsers_name}'")
+        log_info(f"CWD is '{os.getcwd()}'")
+        rc = 0
+        if cmd_name == _RUN_CMD:
+            run_info = actions.run_tests(args.conf, args.test, args.group, args.require_group,
+                                         args.exclude_group, args.repeat, args.show_summary,
+                                         args.debug)
+            if run_info.failed_tests:
+                rc += 1
+            if run_info.not_run_tests:
+                rc += 2
+        elif cmd_name == _LIST_CMD:
+            actions.list_tests(args.conf, args.names_only, args.group, args.require_group,
+                               args.exclude_group, args.all)
+        elif cmd_name == _GROUPS_CMD:
+            actions.list_groups(args.conf)
+        elif cmd_name == _INFO_CMD:
+            actions.show_test_info(args.conf, args.test_name, args.expand)
+        else:
+            raise XeetException(f"Unknown command '{cmd_name}'")
+        return rc
 
     except XeetException as e:
-        log_error(f"xeet: {e}", pr=True, file=sys.stderr)
+        # flush the stdout buffer
+        log_error(f"xeet: {e}")
         return 255
-    return 0
 
 
 if __name__ == "__main__":
