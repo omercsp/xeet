142225dcc019ca030d80b0e9a4d735203715b8ea
Omer Caspi <omer.caspi@gmail.com>
2024-12-06 13:24:54 +0200

    ~~~WIP~~~ xeet: xstep
    Signed-off-by: Omer Caspi <omer.caspi@gmail.com>

--- src/ut/test_config.py
+++ src/ut/test_config.py
@@ -1,6 +1,6 @@
-from ut import unittest, ConfigTestWrapper
-from xeet.config import (read_config_file, TestCriteria, _extract_include_files,
-                         XeetIncludeLoopException)
+from ut import ConfigTestWrapper
+from ut.dummy_test_config import DummyTestConfig, gen_dummy_step_desc, DummyStep
+from xeet.config import TestCriteria, _extract_include_files, XeetIncludeLoopException
 from xeet.xtest import Xtest
 
 
@@ -10,21 +10,12 @@ _TEST2 = "test2"
 _TEST3 = "test3"
 _TEST4 = "test4"
 
-_MAIN_CONF = "xeet_main.json"
 _INC_CONF0 = "xeet_inc0.json"
 _INC_CONF1 = "xeet_inc1.json"
 _ALL_TESTS_CRIT = TestCriteria([], [], [], [], True)
 
 
-class TestConfig(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        ConfigTestWrapper.init_xeet_dir()
-
-    @classmethod
-    def tearDownClass(cls):
-        ConfigTestWrapper.fini_xeet_dir()
-
+class TestConfig(DummyTestConfig):
     def test_inclusion_lists(self):
         CONF0_0 = "conf0.0.json"
         CONF1_0 = "conf1.0.json"
@@ -108,11 +99,9 @@ class TestConfig(unittest.TestCase):
                              conf2.file_path, conf1.file_path, conf0.file_path])
 
     def test_get_xtest_by_name_simple(self):
-        conf_wrapper = ConfigTestWrapper(_MAIN_CONF)
-        conf_wrapper.add_test(_TEST0)
-        conf_wrapper.save()
+        self.set_test(_TEST0, reset=True, save=True)
 
-        config = read_config_file(conf_wrapper.file_path)
+        config = self.config_file()
         xtest = config.xtest(_TEST0)
         self.assertIsInstance(xtest, Xtest)
         assert xtest is not None
@@ -122,14 +111,12 @@ class TestConfig(unittest.TestCase):
         self.assertIsNone(xtest)
 
     def test_get_xtest_by_name(self):
-        conf_wrapper = ConfigTestWrapper(_MAIN_CONF)
-        conf_wrapper.add_test(_TEST0)
-        conf_wrapper.add_test(_TEST1)
-        conf_wrapper.add_test(_TEST2)
-        conf_wrapper.add_test(_TEST3)
-        conf_wrapper.save()
+        self.set_test(_TEST0, reset=True)
+        self.set_test(_TEST1)
+        self.set_test(_TEST2)
+        self.set_test(_TEST3, save=True)
 
-        config = read_config_file(conf_wrapper.file_path)
+        config = self.config_file()
         crit = TestCriteria([_TEST0], [], [], [], True)
         xtests = config.xtests(criteria=crit)
         self.assertEqual(len(xtests), 1)
@@ -150,14 +137,12 @@ class TestConfig(unittest.TestCase):
         GROUP1 = "group1"
         GROUP2 = "group2"
 
-        conf_wrapper = ConfigTestWrapper(_MAIN_CONF)
-        conf_wrapper.add_test(_TEST0, groups=[GROUP0])
-        conf_wrapper.add_test(_TEST1, groups=[GROUP1])
-        conf_wrapper.add_test(_TEST2, groups=[GROUP2])
-        conf_wrapper.add_test(_TEST3, groups=[GROUP0, GROUP1])
-        conf_wrapper.save()
+        self.set_test(_TEST0, groups=[GROUP0], reset=True)
+        self.set_test(_TEST1, groups=[GROUP1])
+        self.set_test(_TEST2, groups=[GROUP2])
+        self.set_test(_TEST3, groups=[GROUP0, GROUP1], save=True)
 
-        config = read_config_file(conf_wrapper.file_path)
+        config = self.config_file()
         crit = TestCriteria([], [GROUP0], [], [], True)
         xtests = config.xtests(criteria=crit)
         self.assertEqual(len(xtests), 2)
@@ -188,12 +173,10 @@ class TestConfig(unittest.TestCase):
         self.assertSetEqual(set([t.name for t in xtests]), set([_TEST1, _TEST2, _TEST3]))
 
     def test_get_all_tests(self):
-        conf_wrapper = ConfigTestWrapper(_MAIN_CONF)
-        conf_wrapper.add_test(_TEST0)
-        conf_wrapper.add_test(_TEST1)
-        conf_wrapper.add_test(_TEST2)
-        conf_wrapper.save()
-        config = read_config_file(conf_wrapper.file_path)
+        self.set_test(_TEST0, reset=True)
+        self.set_test(_TEST1)
+        self.set_test(_TEST2, save=True)
+        config = self.config_file()
         xtests = config.xtests(criteria=_ALL_TESTS_CRIT)
         self.assertSetEqual(set([t.name for t in xtests]), set([_TEST0, _TEST1, _TEST2]))
 
@@ -203,23 +186,20 @@ class TestConfig(unittest.TestCase):
         included_conf_wrapper.add_test(_TEST4)
         included_conf_wrapper.save()
 
-        conf_wrapper.includes.append(_INC_CONF0)
-        conf_wrapper.save()
+        self.add_include(_INC_CONF0, save=True)
 
-        config = read_config_file(conf_wrapper.file_path)
+        config = self.config_file()
         xtests = config.xtests(criteria=_ALL_TESTS_CRIT)
         self.assertSetEqual(set([t.name for t in xtests]),
                             set([_TEST0, _TEST1, _TEST2, _TEST3, _TEST4]))
 
     def test_get_hidden_tests(self):
-        conf_wrapper = ConfigTestWrapper(_MAIN_CONF)
-        conf_wrapper.add_test(_TEST0)
-        conf_wrapper.add_test(_TEST1, abstract=True)
-        conf_wrapper.add_test(_TEST2)
-        conf_wrapper.save()
+        self.set_test(_TEST0, reset=True)
+        self.set_test(_TEST1, abstract=True)
+        self.set_test(_TEST2, save=True)
 
         crit = TestCriteria([], [], [], [], True)
-        config = read_config_file(conf_wrapper.file_path)
+        config = self.config_file()
         xtests = config.xtests(criteria=crit)
         self.assertSetEqual(set([t.name for t in xtests]), set([_TEST0, _TEST1, _TEST2]))
 
@@ -228,36 +208,41 @@ class TestConfig(unittest.TestCase):
         self.assertSetEqual(set([t.name for t in xtests]), set([_TEST0, _TEST2]))
 
     def test_shadowed_tests(self):
-        main_conf_wrapper = ConfigTestWrapper(_MAIN_CONF, includes=[_INC_CONF0])
-        main_conf_wrapper.add_test(_TEST0)
-        main_conf_wrapper.add_test(_TEST1, cmd="main config")
-        main_conf_wrapper.save()
+        def assert_test(name: str, dummy_field: str) -> None:
+            xtest = self.config_file().xtest(name)
+            self.assertIsInstance(xtest, Xtest)
+            assert xtest is not None
+            assert xtest.run_steps is not None
+            self.assertEqual(len(xtest.run_steps), 1)
+            self.assertIsInstance(xtest.run_steps[0], DummyStep)
+            self.assertEqual(xtest.run_steps[0].dummy_model.dummy_field,   # type: ignore
+                             dummy_field)
+
+        depth0 = "depth0"
+        depth1 = "depth1"
+        depth2 = "depth2"
+
+        depth0_step = gen_dummy_step_desc(dummy_field=depth0)
+        depth1_step = gen_dummy_step_desc(dummy_field=depth1)
+        depth2_step = gen_dummy_step_desc(dummy_field=depth2)
+
+        self.set_test(_TEST0, run=[depth0_step], reset=True)
+        self.set_test(_TEST1, run=[depth0_step])
+        self.add_include(_INC_CONF0, save=True)
 
         conf_wrapper = ConfigTestWrapper(_INC_CONF0, includes=[_INC_CONF1])
-        conf_wrapper.add_test(_TEST1, cmd="included config")
-        conf_wrapper.add_test(_TEST2, cmd="included config")
-        conf_wrapper.save()
+        conf_wrapper.add_test(_TEST1, run=[depth1_step])
+        conf_wrapper.add_test(_TEST2, run=[depth1_step], save=True)
 
         conf_wrapper = ConfigTestWrapper(_INC_CONF1)
-        conf_wrapper.add_test(_TEST2, cmd="deep included config")
-        conf_wrapper.add_test(_TEST3, cmd="deep included config")
-        conf_wrapper.save()
+        conf_wrapper.add_test(_TEST2, run=[depth2_step])
+        conf_wrapper.add_test(_TEST3, run=[depth2_step], save=True)
 
-        config = read_config_file(main_conf_wrapper.file_path)
+        config = self.config_file()
         xtests = config.xtests(criteria=_ALL_TESTS_CRIT)
         self.assertSetEqual(set([t.name for t in xtests]), set([_TEST0, _TEST1, _TEST2, _TEST3]))
 
-        xtest = config.xtest(_TEST1)
-        self.assertIsInstance(xtest, Xtest)
-        assert xtest is not None
-        self.assertEqual(xtest.cmd, "main config")
-
-        xtest = config.xtest(_TEST2)
-        self.assertIsInstance(xtest, Xtest)
-        assert xtest is not None
-        self.assertEqual(xtest.cmd, "included config")
-
-        xtest = config.xtest(_TEST3)
-        self.assertIsInstance(xtest, Xtest)
-        assert xtest is not None
-        self.assertEqual(xtest.cmd, "deep included config")
+        assert_test(_TEST0, depth0)
+        assert_test(_TEST1, depth0)
+        assert_test(_TEST2, depth1)
+        assert_test(_TEST3, depth2)
---
+++ src/xeet/xstep.py
@@ -0,0 +1,135 @@
+from xeet.common import XeetVars, XeetException
+from xeet.log import log_info
+from pydantic import BaseModel, ConfigDict, Field
+from dataclasses import dataclass, field
+from typing import Callable
+from timeit import default_timer as timer
+
+
+class XStepModel(BaseModel):
+    model_config = ConfigDict(extra='forbid')
+    step_type: str = Field(validation_alias="type")
+    name: str = ""
+
+
+class XeetStepException(XeetException):
+    ...
+
+
+#  This class is used to pass arguments to the step classes
+@dataclass
+class XStepTestArgs:
+    log_info: Callable = log_info
+    debug_mode: bool = False
+    output_dir: str = ""
+    stage_prefix: str = ""
+    index: int = 0
+
+
+class XeetStepInitException(XeetStepException):
+    def __init__(self, error: str, step_type, args: XStepTestArgs) -> None:
+        super().__init__(error)
+        self.step_type = step_type
+        self.prefix = args.stage_prefix
+        self.index = args.index
+
+    def __str__(self) -> str:
+        ret = f"Error initializing {self.prefix} step {self.index}"
+        if self.step_type:
+            ret += f" ({self.step_type})"
+        return f"{ret}:\n{self.error}"
+
+
+@dataclass
+class XStepResult:
+    step: "XStep"
+    err_summary: str = ""
+    completed: bool = False
+    duration: float | None = None
+    failed: bool = False
+
+    def error_summary(self) -> str:
+        ret = f"{self.step.stage_prefix} step {self.step.index}"
+        if self.step.model.name:
+            ret += f" ('{self.step.model.name}')"
+        if not self.completed:
+            ret += f" incomplete: {self.err_summary}"
+        if self.failed:
+            ret += f" failed: {self.err_summary}"
+        return ret
+
+
+class XStep:
+    @staticmethod
+    def model_class() -> type[XStepModel]:
+        return XStepModel
+
+    @staticmethod
+    def result_class() -> type[XStepResult]:
+        return XStepResult
+
+    def __init__(self, model: XStepModel, args: XStepTestArgs):
+        self.model = model
+        self.test_settings = args
+        self.log_info = args.log_info
+        self.index = args.index
+        self.stage_prefix = args.stage_prefix
+
+    def expand(self, _: XeetVars) -> str:
+        ...
+
+    def run(self) -> XStepResult:
+        res = self.result_class()(self)
+        start = timer()
+        res.completed = self._run(res)
+        res.duration = timer() - start
+        self.log_info(f"step finished with in {res.duration:.3f}s")
+        return res
+
+    def print_name(self) -> str:
+        ret = f"{self.stage_prefix} step {self.index}: {self.model.step_type}"
+        if self.model.name:
+            ret += f" ('{self.model.name}')"
+        return ret
+
+    def summary(self) -> str:
+        return self.print_name()
+
+    def _run(self, _: XStepResult) -> bool:
+        raise NotImplementedError
+
+
+@dataclass
+class XStepListResult:
+    results: list[XStepResult] = field(default_factory=list)
+    completed: bool = True
+    failed: bool = False
+
+    def error_summary(self) -> str:
+        return "\n".join([r.error_summary() for r in self.results if r.failed])
+
+    #  post_init is called after the dataclass is initialized. This is used
+    #  in unittesting only. By default, results is empty, so completed and failed
+    def __post_init__(self) -> None:
+        if not self.results:
+            return
+        self.completed = all([r.completed for r in self.results])
+        self.failed = any([r.failed for r in self.results])
+
+
+#  stop_on_err will stop if either a step fails or is incomplete
+def run_xstep_list(step_list: list[XStep] | None, res: XStepListResult, stop_on_err: bool) -> None:
+    if not step_list:
+        return
+
+    for step in step_list:
+        step_res = step.run()
+        res.results.append(step_res)
+        if not step_res.completed:
+            res.completed = False
+            if stop_on_err:
+                break
+        if step_res.failed:
+            res.failed = True
+            if stop_on_err:
+                break
