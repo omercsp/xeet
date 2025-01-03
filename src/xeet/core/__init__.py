47eb70d91b39d4eb22603e6b0ca2da3f11e86d3d
Omer Caspi <omer.caspi@gmail.com>
2025-01-03 23:44:29 +0200

    core: platform support
    - Add platform list attribute to tests. If existing, test will only run
      if the running platform is compatible.
    - Add a platform auto variable XG_PLATFORM, allowing, among other
      things, inclusion of a platform specific configuration file.
    
    Signed-off-by: Omer Caspi <omer.caspi@gmail.com>

--- src/xeet/core/__init__.py
+++ src/xeet/core/__init__.py
@@ -69,6 +69,7 @@ class RuntimeInfo:
 
         self.xvars = XeetVars(start_vars=XeetVarsModel({
             system_var_name("CWD"): self.cwd,
+            system_var_name("PLATFORM"): os.name.lower(),
         }))
         self.defs_dict = {}
         self.debug_mode = False
