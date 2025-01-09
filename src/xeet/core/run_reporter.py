94adb28cc93ceca88934111fc1f0076e4ba91cb1
Omer Caspi <omer.caspi@gmail.com>
2025-01-10 01:29:34 +0200

    xeet: parallel test execution
    Run each iteration win n-threads. This requires synchronization of all
    the supporting data structures so parallel tests won't overflow one
    another.
    
    Signed-off-by: Omer Caspi <omer.caspi@gmail.com>

--- src/xeet/core/run_reporter.py
+++ src/xeet/core/run_reporter.py
@@ -1,3 +1,6 @@
+from threading import Lock
+
+
 #  Base class for run reporters (CLI, REST, etc.)
 class RunReporter:
     def __init__(self) -> None:
@@ -44,6 +47,7 @@ class RunNotifier:
     def __init__(self,  reporters: list[RunReporter]) -> None:
         super().__init__()
         self.reporters: list[RunReporter] = reporters
+        self._lock = Lock()
 
     #  Global events
     def on_run_start(self, run_res, tests: list) -> None:
@@ -70,27 +74,32 @@ class RunNotifier:
 
     #  Test events
     def on_test_start(self, test) -> None:
-        for r in self.reporters:
-            r.on_test_start(test)
+        with self._lock:
+            for r in self.reporters:
+                r.on_test_start(test)
 
     def on_test_end(self, test, test_res) -> None:
-        for r in self.reporters:
-            r.on_test_end(test, test_res)
+        with self._lock:
+            for r in self.reporters:
+                r.on_test_end(test, test_res)
 
     def on_phase_start(self, test, phase_name: str, steps_count: int) -> None:
-        for r in self.reporters:
-            r.on_phase_start(test, phase_name, steps_count)
+        with self._lock:
+            for r in self.reporters:
+                r.on_phase_start(test, phase_name, steps_count)
 
     def on_phase_end(self, test, phase_name, steps_count: int) -> None:
-        for r in self.reporters:
-            r.on_phase_end(test, phase_name, steps_count)
+        with self._lock:
+            for r in self.reporters:
+                r.on_phase_end(test, phase_name, steps_count)
 
     # Step events
-
     def on_step_start(self, test, phase_name: str, step, step_index: int) -> None:
-        for r in self.reporters:
-            r.on_step_start(test, phase_name, step, step_index)
+        with self._lock:
+            for r in self.reporters:
+                r.on_step_start(test, phase_name, step, step_index)
 
     def on_step_end(self, test, phase_name: str, step, step_index: int, step_res) -> None:
-        for r in self.reporters:
-            r.on_step_end(test, phase_name, step, step_index, step_res)
+        with self._lock:
+            for r in self.reporters:
+                r.on_step_end(test, phase_name, step, step_index, step_res)
