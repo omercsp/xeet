2b2f7c0d20387ddfca1445c1935e38f147cc1a7e
Omer Caspi <omer.caspi@gmail.com>
2025-01-10 01:29:34 +0200

    xeet: parallel execution
    Run each iteration win n-threads. This requires synchronization of all
    the supporting data structures so parallel tests won't overflow one
    another.
    
    Signed-off-by: Omer Caspi <omer.caspi@gmail.com>

--- src/xeet/core/api.py
+++ src/xeet/core/api.py
@@ -1,13 +1,14 @@
 from . import TestsCriteria
 from .test import Test, TestModel
 from .result import (TestResult, TestPrimaryStatus, TestSecondaryStatus, RunResult, TestStatus,
-                     EmptyRunResult)
+                     EmptyRunResult, IterationResult)
 from .driver import XeetModel, xeet_init
-from .run_reporter import RunReporter
-from xeet.log import log_info, log_verbose
+from .run_reporter import RunNotifier, RunReporter
 from xeet import XeetException
 from timeit import default_timer as timer
 from enum import Enum
+from xeet.log import log_info, log_verbose
+from threading import Lock, Thread, Event
 
 
 def fetch_xtest(config_path: str, name: str, setup: bool = False) -> Test | None:
@@ -53,24 +54,87 @@ def fetch_schema(schema_type: str) -> dict:
     raise XeetException(f"Invalid dump type: {schema_type}")
 
 
-def _run_test(test: Test) -> TestResult:
-    if test.error:
-        return TestResult(TestStatus(TestPrimaryStatus.NotRun, TestSecondaryStatus.InitErr),
-                          status_reason=test.error)
+class _TestsPool:
+    def __init__(self, tests: list[Test]) -> None:
+        self.tests = tests
+        self._index = 0
+        self._lock = Lock()
 
-    log_info(f"Running test '{test.name}'")
-    start = timer()
-    ret = test.run()
-    ret.duration = timer() - start
-    return ret
+    def get_next(self) -> Test | None:
+        with self._lock:
+            if self._index >= len(self.tests):
+                return None
+            ret = self.tests[self._index]
+            self._index += 1
+            return ret
+
+    def reset(self) -> None:
+        self._index = 0
+
+
+class _TestRunner(Thread):
+    runner_id_count = 0
+    runner_error = Event()
+
+    @staticmethod
+    def reset() -> None:
+        _TestRunner.runner_error.clear()
+        _TestRunner.runner_id_count = 0
+
+    def __init__(self, pool: _TestsPool, notifier: RunNotifier, iter_res: IterationResult,
+                 iteration: int) -> None:
+        super().__init__()
+        self.pool = pool
+        self.notifier = notifier
+        self.iter_res = iter_res
+        self._stop_event = Event()
+        self.runner_id = f"r{iteration}.{_TestRunner.runner_id_count}"
+        _TestRunner.runner_id_count += 1
+        self.error: XeetException | None = None
+
+    def log_info(self, *args, **kwargs) -> None:
+        log_info(f"{self.runner_id}:", *args, **kwargs, depth=1)
+
+    def run(self) -> None:
+        while True:
+            if self._stop_event.is_set() or _TestRunner.runner_error.is_set():
+                self.log_info("Stopping")
+                break
+            test = self.pool.get_next()
+            if test is None:
+                self.log_info("No more tests, goodbye")
+                break
+            self.log_info(f"got test '{test.name}'")
+            self.notifier.on_test_start(test)
+            try:
+                test_res = self._run_test(test)
+            except XeetException as e:
+                self.log_info(f"Error occurred during test '{test.name}': {e}")
+                self.error = e
+                _TestRunner.runner_error.set()
+                break
+
+            self.iter_res.add_test_result(test.name, test_res)
+            self.notifier.on_test_end(test, test_res)
+
+    def _run_test(self, test: Test) -> TestResult:
+        if test.error:
+            return TestResult(TestStatus(TestPrimaryStatus.NotRun, TestSecondaryStatus.InitErr),
+                              status_reason=test.error)
+
+        start = timer()
+        ret = test.run()
+        ret.duration = timer() - start
+        return ret
 
 
 def run_tests(conf: str,
               criteria: TestsCriteria,
               reporters: RunReporter | list[RunReporter],
               debug_mode: bool = False,
+              threads: int = 1,
               iterations: int = 1) -> RunResult:
-    log_info("Starting run", pr_suffix="------------\n")
+    log_info("Starting xeet session", pr_suffix="------------\n")
     if not isinstance(reporters, list):
         reporters = [reporters]
     driver = xeet_init(conf, debug_mode, reporters)
@@ -83,7 +147,11 @@ def run_tests(conf: str,
     run_res = RunResult(iterations=iterations, criteria=criteria)
 
     log_info("Tests run list: {}".format(", ".join([x.name for x in tests])))
+    log_info(f"Using {threads} threads per iteration")
+
+    run_res = RunResult(iterations=iterations, criteria=criteria)
     notifier.on_run_start(run_res, tests)
+    tests_pool = _TestsPool(tests)
 
     for iter_n in range(iterations):
         iter_res = run_res.iter_results[iter_n]
@@ -91,12 +159,17 @@ def run_tests(conf: str,
             log_info(f">>> Iteration {iter_n}/{iterations - 1}")
         driver.rti.set_iteration(iter_n, iterations)
         notifier.on_iteration_start(iter_res, iter_n)
-        for test in tests:
-            notifier.on_test_start(test)
-            test_res = _run_test(test)
-            iter_res.add_test_result(test.name, test_res)
-            notifier.on_test_end(test, test_res)
-        notifier.on_iteration_end()
+        _TestRunner.reset()
+        runners = [_TestRunner(tests_pool, notifier, iter_res, iter_n) for _ in range(threads)]
+        for runner in runners:
+            runner.start()
+        for runner in runners:
+            runner.join()
+        if _TestRunner.runner_error.is_set():
+            log_info("Error occurred during run")
+            first_error = next((r.error for r in runners if r.error), None)
+            if first_error:
+                raise first_error
 
         if iterations > 1:
             log_info(f"Finished iteration #{iter_n}/{iterations - 1}")
@@ -105,5 +178,7 @@ def run_tests(conf: str,
                 continue
             test_list_str = ", ".join(test_names)
             log_info(f"{status}: {test_list_str}")
+        notifier.on_iteration_end()
+        tests_pool.reset()
     notifier.on_run_end()
     return run_res
