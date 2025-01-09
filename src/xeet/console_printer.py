e568973b4302100c455616f1c220cb943a3806cd
Omer Caspi <omer.caspi@gmail.com>
2025-01-10 01:29:34 +0200

    xeet: parallel execution
    Run each iteration win n-threads. This requires synchronization of all
    the supporting data structures so parallel tests won't overflow one
    another.
    
    Signed-off-by: Omer Caspi <omer.caspi@gmail.com>

--- src/xeet/console_printer.py
+++ src/xeet/console_printer.py
@@ -1,4 +1,5 @@
-from xeet.core.events import EventReporter
+from xeet.core.events import LockableEventReporter
+from xeet.common import locked
 from xeet.pr import *
 from xeet.core.test import (TestPrimaryStatus, TestResult, TestStatus, TestSecondaryStatus, Test,
                             Phase)
@@ -51,6 +52,7 @@ class ConsoleDisplayOpts:
     summary: bool = True
     iteration_summary: bool = False
     detailed_summary: bool = True
+    threads_header: bool = False
 
     _verbose_criterira: bool = False
 
@@ -60,6 +62,7 @@ class ConsoleDisplayOpts:
         self.iteration_summary = True
         self.test_timing = True
         self._verbose_criterira = True
+        self.threads_header = True
 
     def set_concise(self):
         self.header = False
@@ -74,6 +77,7 @@ class ConsoleDisplayOpts:
         self.tests = False
         self.result_details = False
         self.ongoing = False
+        self.threads_header = False
 
     @classmethod
     @cache
@@ -91,7 +95,7 @@ class ConsoleDisplayOpts:
 
 
 @dataclass
-class ConsolePrinter(EventReporter):
+class ConsolePrinter(LockableEventReporter):
     live: Live = None  # type: ignore
     display: ConsoleDisplayOpts = field(default_factory=ConsoleDisplayOpts)
 
@@ -116,6 +120,8 @@ class ConsolePrinter(EventReporter):
                 pr_info("Running tests: {}\n".format(", ".join([x.name for x in self.tests])))
             else:
                 pr_info("No tests to run\n")
+        if self.display.threads_header:
+            pr_info(f"Threads: {self.threads} per iteration\n")
 
     def _print_criteria(self) -> None:
         if not self.display.criteria:
@@ -166,10 +172,12 @@ class ConsolePrinter(EventReporter):
 
         pr_info("\n".join(lines) + "\n")
 
+    @locked
     def on_test_start(self, test: Test) -> None:
         self.curr_tests.append(test.name)
         self._print_curr_tests()
 
+    @locked
     def on_test_end(self, test_res: TestResult) -> None:
         if not self.display.tests:
             return
@@ -273,6 +281,7 @@ class ConsolePrinter(EventReporter):
                               underline_char='-'))
 
         pr_info(f"Total iterations: {self.iterations}")
+        pr_info(f"Threads used per iteration: {self.threads}")
         detailed = self.display.detailed_summary and not show_iteration_summary
         self._summarize_result_names(total_summary, detailed, self.run_res.duration)
 
@@ -281,7 +290,7 @@ _pr_debug_title = create_print_func("orange1", LogLevel.ALWAYS)
 
 
 @dataclass
-class DebugPrinter(EventReporter):
+class DebugPrinter(LockableEventReporter):
     def _step_title(self, step: Step, phase_name: str, step_index: int,
                     sentence_start: bool = False) -> str:
         if sentence_start:
@@ -296,9 +305,11 @@ class DebugPrinter(EventReporter):
     def on_run_start(self, **_) -> None:
         _pr_debug_title("Starting run")
 
+    @locked
     def on_test_start(self, test: Test) -> None:
         _pr_debug_title(f">>>>>>> Starting test '{test.name}' <<<<<<<")
 
+    @locked
     def on_test_end(self, test_res: TestResult) -> None:
         test = test_res.test
         _pr_debug_title(f"Test '{test.name}' ended. (status: {test_res.status.primary}, "
@@ -308,10 +319,12 @@ class DebugPrinter(EventReporter):
         if test_res.status.primary == TestPrimaryStatus.Failed:
             pr_error(f"Test failed")
 
+    @locked
     def on_step_start(self, step: Step) -> None:
         title = self._step_title(step, step.phase.name, step.step_index, sentence_start=True)
         _pr_debug_title(f"{title} - staring run")
 
+    @locked
     def on_step_end(self, step_res: StepResult) -> None:
         step = step_res.step
         text = self._step_title(step, step.phase.name, step.step_index, sentence_start=True)
@@ -320,6 +333,7 @@ class DebugPrinter(EventReporter):
         text += f"duration: {step_res.duration:.3f}s)"
         _pr_debug_title(text)
 
+    @locked
     def on_phase_start(self, phase: Phase) -> None:
         steps_count = len(phase.steps)
         if steps_count == 0:
@@ -327,6 +341,7 @@ class DebugPrinter(EventReporter):
             return
         _pr_debug_title(f"Starting {phase.name} phase run, {steps_count} step(s)")
 
+    @locked
     def on_phase_end(self, phase_res: PhaseResult) -> None:
         phase = phase_res.phase
         if not phase.steps:
@@ -335,9 +350,11 @@ class DebugPrinter(EventReporter):
         _pr_debug_title(f"{text} phase ended")
 
     # General event message
+    @locked
     def on_test_message(self, _: Test, msg: str, *args, **kwargs) -> None:
         self._print_msg(msg, *args, **kwargs)
 
+    @locked
     def on_step_message(self, _: Step, msg: str, *args, **kwargs) -> None:
         self._print_msg(msg, *args, **kwargs)
 
