764c7fb359af88ae541bf0ee187baffd117f4b04
Omer Caspi <omer.caspi@gmail.com>
2025-01-16 17:47:56 +0200

    unit: base tests
    Signed-off-by: Omer Caspi <omer.caspi@gmail.com>

--- src/xeet/core/test.py
+++ src/xeet/core/test.py
@@ -46,6 +46,7 @@ class TestModel(KeysBaseModel):
 
     # Internals
     error: str = Field(_EMPTY_STR, exclude=True)
+    __test__ = False
 
     @model_validator(mode='after')
     def post_validate(self) -> "TestModel":
@@ -106,6 +107,8 @@ class Phase:
 
 
 class Test:
+    __test__ = False
+
     def __init__(self, model: TestModel, rti: RuntimeInfo) -> None:
         self.model = model
         self.rti = rti
