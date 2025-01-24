6195fe9392142809102a841b059be374ab2aa404
Omer Caspi <omer.caspi@gmail.com>
2025-01-24 23:00:41 +0200

    xeet: test randomization
    Add support for test order randomization per test run iteration;
    includes a CLI flag.
    
    Signed-off-by: Omer Caspi <omer.caspi@gmail.com>

--- src/xeet/core/api.py
+++ src/xeet/core/api.py
@@ -10,6 +10,7 @@ from typing import Callable
 from enum import Enum
 from threading import Thread, Event, Condition
 import signal
+import random
 
 
 def fetch_test(config_path: str, name: str) -> Test | None:
@@ -62,12 +63,13 @@ def _gen_notify_func(notifier: EventNotifier) -> Callable:
 
 
 class _TestsPool:
-    def __init__(self, tests: list[Test], threads: int) -> None:
+    def __init__(self, tests: list[Test], threads: int, randomize: bool) -> None:
         self._base_tests = tests
         self.threads = threads
         self._tests: list[Test] = []
         self.condition = Condition()
         self.abort = Event()
+        self.randomize = randomize
         self.reset()
 
     def stop(self) -> None:
@@ -132,6 +134,8 @@ class _TestsPool:
 
     def reset(self) -> None:
         self._tests = self._base_tests.copy()
+        if self.randomize:
+            random.shuffle(self._tests)
 
 
 class _TestRunner(Thread):
@@ -241,6 +245,7 @@ def run_tests(conf: str,
               reporters: EventReporter | list[EventReporter],
               debug_mode: bool = False,
               threads: int = 1,
+              randomize: bool = False,
               iterations: int = 1) -> RunResult:
     driver = xeet_init(conf, debug_mode)
     rti = driver.rti
@@ -264,7 +269,7 @@ def run_tests(conf: str,
     signal.signal(signal.SIGINT, _user_break)
     run_res.set_start_time()
 
-    tests_pool = _TestsPool(tests, threads)
+    tests_pool = _TestsPool(tests, threads, randomize=randomize)
     for iter_n in range(iterations):
         _run_iter(rti, tests_pool, matrix, iter_n, run_res, threads)
         tests_pool.reset()
